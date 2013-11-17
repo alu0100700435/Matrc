cd89847981b03c5956b161709e4bd65cbc9e5f31
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-11-17 14:40:34 +0000

    cambiando fraction para operaciones con numeros enteros

---
+++ lib/matrc/fraction.rb
@@ -0,0 +1,89 @@
+require "gcd.rb"
+
+include Comparable
+
+class Fraction
+  
+  attr_accessor :num, :den
+  
+  def initialize(num, den)
+    
+    com = gcd(num, den)
+    if (den < 0)
+      @num, @den = ((-1*num)/com), ((-1*den)/com)
+    else
+      @num, @den = num/com, den/com
+    end
+  end
+  
+  def to_s()
+    "#{@num}/#{@den}"
+  end
+ 
+   def to_f()
+     flotante =@num.to_f/@den.to_f
+     flotante
+   end
+  
+   def +(other)
+     if !(other.is_a? Fraction)
+		other = Fraction.new(other,1)
+	 end
+	  Fraction.new(@num* other.den + other.num*@den, @den * other.den)
+   end
+   
+  def -(other)
+    if !(other.is_a? Fraction)
+		other = Fraction.new(other,1)
+	end
+    Fraction.new(@num* other.den - other.num*@den, @den * other.den)
+  end
+ 
+  def *(other)
+    if !(other.is_a? Fraction)
+		other = Fraction.new(other,1)
+	end
+     Fraction.new(@num * other.num, @den *other.den)
+  end
+  
+  def /(other)
+    if !(other.is_a? Fraction)
+		other = Fraction.new(other,1)
+	end
+    Fraction.new(@num * other.den, @den *other.num)
+  end
+  
+  def %(other)
+    if !(other.is_a? Fraction)
+		other = Fraction.new(other,1)
+	end
+    a = (@num*other.den)
+    b = (@den * other.num)
+    c = a%b
+    c
+  end
+  
+  def abs()
+     Fraction.new(@num.abs, @den.abs)
+  end
+  
+  def -@()
+    Fraction.new(@num * -1, @den)
+  end
+   
+   def reciprocal()
+     Fraction.new(@den, @num)
+   end
+       
+   def <=>(other)
+    if !(other.is_a? Fraction)
+		other = Fraction.new(other,1)
+	end
+     to_float() <=> other.to_float()
+   end
+   
+   def coerce(other)
+        [self, other]
+    end
+        
+end
