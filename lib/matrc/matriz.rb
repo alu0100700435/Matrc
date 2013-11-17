52566abfd078805f58a70e2f3d9ec603d9a563f8
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-11-17 15:11:20 +0000

    creación de la clase madre

---
+++ lib/matrc/matriz.rb
@@ -0,0 +1,62 @@
+class Matriz
+  
+  attr_reader :row, :col
+  
+  include Enumerable
+
+  def initialize(row,col)
+    @row,@col = row,col
+   end
+   
+  def *(other)
+    if(self.col == other.row)
+      matres = self.class.new(self.row,self.col)
+      for i in 0...self.row
+        for j in 0...other.col
+          for k in 0...self.col
+            matres[i,j] += self[i,k] * other[k,j]
+          end
+        end
+      end
+      matres
+	end
+  end
+  
+  def +(other)
+    if(self.col == other.col && other.row == self.row)
+      matres = self.class.new(self.row,self.col)
+      for i in 0...self.row
+        for j in 0...self.col
+          matres[i,j] = self[i,j] + other[i,j]
+        end
+      end
+      matres
+	end  
+  end
+  
+  def -(other)
+    if(self.col == other.col && other.row == self.row)
+      matres = self.class.new(self.row,self.col)
+      for i in 0...self.row
+        for j in 0...self.col
+          matres[i,j] = self[i,j] - other[i,j]
+        end
+      end
+      matres
+	end      
+  end
+
+  def coerce(other)
+    [self, other]
+  end
+
+  def each
+    for i in 0...self.row
+	  for j in 0...self.col
+	    yield self[i, j]
+      end
+	end
+  end
+  
+  
+end
