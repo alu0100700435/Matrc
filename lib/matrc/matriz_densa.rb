f75fe322604bc4161b8aff9173f7b989d928c759
Raquel Alvarez <alu4367@cc387.(none)>
2013-11-18 10:41:46 +0000

    construyendo la clase de  matriz densa

--- lib/matrc/matriz_densa.rb
+++ lib/matrc/matriz_densa.rb
@@ -5,5 +5,25 @@ class MatrizDensa < Matriz
     super(row,col)
     @mtdensa = Array.new(row){Array.new(col, 0}
   end
+  
+  def [](row,col)
+    @mtdensa[row][col]
+  end
+  
+  def []=(row,col,val)
+    @mtdensa[row][col] = val
+  end
+
+  def ==(other)
+    for i in 0...@row
+      for j in 0...@col
+        if(@mtdensa[i][j] == other[i}[j])
+	  return true
+	else
+          return false
+        end
+       end
+     end
+  end
 
 end
