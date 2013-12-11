af56d991115e8bd89e784ea9d6c5642b675b066d
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-12-11 15:45:16 +0000

    algunos cambios

--- lib/matrc/matriz_DSL.rb
+++ lib/matrc/matriz_DSL.rb
@@ -1,57 +1,62 @@
 #clase MatrizDSL
+module Matrc
 
-class MatrizDSL 
+  class MatrizDSL 
 
-  attr_accessor :operation, :op, :mostrar, :tipo
-  
-  def initialize(operation, &block)
-    @operation = operation
-    @op = []
-    @tipo = ""
-#    @mostrar = ""
+    attr_accessor :operation, :operandos, :mostrar, :tipo
     
-     
-   instance_eval(&block)
-   
-#   case mostrar
- #    when "fichero"
-  #     then 
-   #      File.open('mostrar.txt', 'mostrar') do |x|
-    #       x.puts self
-   #      end
-  #   when "consola" then puts self 
- #    end
-#   end
-   end
-   
-   def calculo  
-    case @operation
-      when "Suma" 
-        return @op.reduce(:+)  
-   
-     end 
-   end    
+    def initialize(operation, &block)
+      @operation = operation
+      @operandos= []
+      @tipo = ""
+      @mostrar = ""
       
-      
-   def operando(matrix)
-    @op.push matrixcrear(matrix)
-    
-   end
-  # def opcion(opt)
-   #  @mostrar= opt
-   #end
-   
-   def tipo_matriz(tip)
-     @tipo = tip
-   end
-      
-   def matrixcrear(elementos)
-     elem = MatEntero.new(elementos.size, elementos[0].size)
-     elementos.each_with_index do |x, i|
-      x.each_with_index do |y, j|
-        elem[i, j] = y
+       
+       instance_eval(&block)
+       
+       case mostrar
+         when "fichero"
+           then 
+             File.open('mostrar.txt', 'mostrar') do |x|
+               x.puts self
+             end
+         when "consola" then puts self 
+         end
       end
     end
-    elem
-   end
-   end
+     
+     def calcular  
+      case @operation
+        when "Suma" 
+          return @operandos.reduce(:+)  
+        when "Resta" 
+          return @operandos.reduce(:-)  
+        when "Multiplicacion" 
+          return @operandos.reduce(:*)  
+       end 
+     end    
+        
+        
+     def operando(matriz)
+      @op.push matrizcrear(matriz)      
+     end
+
+     def opcion(opt)
+       @mostrar = opt
+     end
+     
+     def tipo_matriz(tipo)
+       @tipo = tipo
+     end
+        
+     def matrizcrear(elementos)
+       elem = @tipo.new(elementos.size, elementos[0].size)
+       elementos.each_with_index do |x, i|
+        x.each_with_index do |y, j|
+          elem[i, j] = y
+        end
+      end
+      elem
+     end
+    end
+end
